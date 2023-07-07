8ac6b3a45ceef0f6adcd0c1327144dbb1bafee74
viniciosragazzidev <devviniciosragazzi@gmail.com>
2023-07-07 12:54:02 -0300

    FixÇ Melhorandoa  otimização de imagens

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -45,7 +45,7 @@ const HeaderComponent = () => {
         </div>
       </div>
 
-      <div className=" h-full flex flex-1 justify-center items-center  max-sm:hidden   header-right ">
+      <div className=" h-full flex flex-1 justify-center items-center  max-sm:mt-10   header-right ">
         <div className="imgArea max-w-sm">
           <Image
             src={"/pugHero.png"}
